e7155f7612147a2ca86a69dbae5c4503aa116d2e
444eiei <s40433@bj.ac.th>
2025-01-30 20:07:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import * as THREE from './node_modules/three/build/three.module.js';
+import * as THREE from 'https://unpkg.com/three@0.172.0/build/three.module.js';
 import { OrbitControls } from './vendor_mods/three/examples/jsm/controls/OrbitControls.js';
 
 // Scene, Camera, Renderer
